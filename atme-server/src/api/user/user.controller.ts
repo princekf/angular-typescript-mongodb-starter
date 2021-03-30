bfeba894f095234575da0408a7cea1e686c6d46b
Karthik Parameswaran <karthik.parameswaran@inreality.com>
2021-03-30 13:16:10 +0530

    LOGIN

--- atme-server/src/api/user/user.controller.ts
+++ atme-server/src/api/user/user.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response, Router as expresRouter } from 'express';
 import { HTTP_RESPONSE_CODE } from '@atme/Constants';
 import { UserModel, UserDoc } from './user.model';
 import { CommonUtil } from '@ls-base/util/common.util';
-import { UserS } from '@atme/entity/User';
+import { UserS, User } from '@atme/entity/User';
 import { userService } from './user.service';
+import { SIGNUP_API, LOGIN_API } from '@atme/serverAPI';
 
 export const router = expresRouter();
 const { HTTP_OK, HTTP_BAD_REQUEST, FORBIDDEN_USER } = HTTP_RESPONSE_CODE;
@@ -45,4 +46,22 @@ const register = async(request: Request, response: Response) => {
 
 };
 
-router.route('/').post(register);
+const login = async(request: Request, response: Response) => {
+
+  const userR:User = request.body;
+  const userE = await userService.findUserByEmail(userR.email);
+
+  if (!userE || !CommonUtil.isValidPassword({password: userR.password,
+    hash: userE.hash,
+    salt: userE.salt})) {
+
+    response.status(HTTP_BAD_REQUEST).json({message: 'Email or password is wrong. Try again.'});
+    return;
+
+  }
+  response.json({message: 'success'}).status(HTTP_OK);
+
+};
+
+router.route(SIGNUP_API).post(register);
+router.route(LOGIN_API).post(login);
